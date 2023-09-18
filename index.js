ec4c362262765c3e46364b9f6f6113977d23e4be
Christian Clausen <maxclausen@gmail.com>
2023-09-18 10:31:31 +0200

    Moved over

---
+++ index.js
@@ -0,0 +1,119 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.MIME_TYPES = exports.parseStoreFileContent = exports.parseRetrieveFileContent = exports.parseUploadFileInfo = exports.storeFilesThenEmit = exports.requestFileThenEmit = exports.sendToClient = exports.broadcastToChannel = exports.joinChannel = exports.replyFileToOrigin = exports.replyToOrigin = exports.postToRapids = exports.merrymakeService = void 0;
+const ext2mime_1 = require("@merrymake/ext2mime");
+const axios_1 = __importDefault(require("axios"));
+const promises_1 = __importDefault(require("fs/promises"));
+async function merrymakeService(handlers, init) {
+    const action = process.argv[process.argv.length - 2];
+    const handler = handlers[action];
+    if (handler !== undefined) {
+        const envelope = JSON.parse(process.argv[process.argv.length - 1]);
+        handler(getPayload(), envelope);
+    }
+    else if (init !== undefined)
+        await init();
+}
+exports.merrymakeService = merrymakeService;
+function postToRapids(event, payload) {
+    if (payload !== undefined) {
+        return axios_1.default.post(`${process.env.RAPIDS}/${event}`, payload.content, {
+            headers: { "Content-Type": payload.mime.toString() },
+        });
+    }
+    else {
+        return axios_1.default.post(`${process.env.RAPIDS}/${event}`);
+    }
+}
+exports.postToRapids = postToRapids;
+function replyToOrigin(content, mime) {
+    return postToRapids("$reply", { content, mime });
+}
+exports.replyToOrigin = replyToOrigin;
+async function replyFileToOrigin(path, mime) {
+    try {
+        let realMime = mime !== undefined
+            ? mime
+            : (0, ext2mime_1.optimisticMimeTypeOf)(path.substring(path.lastIndexOf(".") + 1));
+        if (realMime === null)
+            throw "Unknown file type. Add mimeType argument.";
+        await postToRapids("$reply", {
+            content: await promises_1.default.readFile(path),
+            mime: realMime,
+        });
+    }
+    catch (e) {
+        throw e;
+    }
+}
+exports.replyFileToOrigin = replyFileToOrigin;
+function joinChannel(channel) {
+    return postToRapids("$join", {
+        content: channel,
+        mime: ext2mime_1.COMMON_MIME_TYPES.txt[0],
+    });
+}
+exports.joinChannel = joinChannel;
+function broadcastToChannel(to, event, payload) {
+    return postToRapids("$broadcast", {
+        content: { to, event, payload },
+        mime: ext2mime_1.COMMON_MIME_TYPES.json[0],
+    });
+}
+exports.broadcastToChannel = broadcastToChannel;
+function sendToClient(to, event, payload) {
+    return postToRapids("$send", {
+        content: { to, event, payload },
+        mime: ext2mime_1.COMMON_MIME_TYPES.json[0],
+    });
+}
+exports.sendToClient = sendToClient;
+function requestFileThenEmit(file, emitEvent, passthrough) {
+    return postToRapids("$retrieve", {
+        content: { file, emit: emitEvent, passthrough },
+        mime: ext2mime_1.COMMON_MIME_TYPES.json[0],
+    });
+}
+exports.requestFileThenEmit = requestFileThenEmit;
+function storeFilesThenEmit(contents, emitEvent, passthrough) {
+    return postToRapids("$store", {
+        content: { contents, emit: emitEvent, passthrough },
+        mime: ext2mime_1.COMMON_MIME_TYPES.json[0],
+    });
+}
+exports.storeFilesThenEmit = storeFilesThenEmit;
+async function parseUploadFileInfo(p) {
+    let result = JSON.parse((await p).toString());
+    return result;
+}
+exports.parseUploadFileInfo = parseUploadFileInfo;
+async function parseRetrieveFileContent(p) {
+    let result = JSON.parse((await p).toString());
+    return { ...result, content: Buffer.from(result.content.data) };
+}
+exports.parseRetrieveFileContent = parseRetrieveFileContent;
+async function parseStoreFileContent(p) {
+    let result = JSON.parse((await p).toString());
+    return result;
+}
+exports.parseStoreFileContent = parseStoreFileContent;
+function getPayload() {
+    return new Promise((resolve, reject) => {
+        let bufs = [];
+        process.stdin.on("data", (data) => {
+            bufs.push(data);
+        });
+        process.stdin.on("end", () => {
+            resolve(Buffer.concat(bufs));
+        });
+    });
+}
+function mapValues(obj) {
+    let result = {};
+    Object.keys(obj).forEach((k) => (result[k] = obj[k][0]));
+    return result;
+}
+exports.MIME_TYPES = mapValues(ext2mime_1.COMMON_MIME_TYPES);
---
+++ index.ts
@@ -0,0 +1,202 @@
+import {
+  MimeType,
+  COMMON_MIME_TYPES,
+  optimisticMimeTypeOf,
+} from "@merrymake/ext2mime";
+import axios, { AxiosResponse } from "axios";
+import fs from "fs/promises";
+
+export module PayloadTypes {
+  export interface UploadFileInfo<T> {
+    files: { originalname: string; name: string; type: string; size: number }[];
+    passthrough: T;
+  }
+  export interface StoreFileInfo<T> {
+    files: string[];
+    passthrough: T;
+  }
+  export interface FileContent<T> {
+    content: { data: number[] };
+    passthrough: T;
+  }
+}
+export type PayloadBufferPromise = Promise<Buffer>;
+
+type Handler = (
+  payloadBuffer: PayloadBufferPromise,
+  envelope: Envelope
+) => void;
+
+export type Envelope = { messageId: string; traceId: string };
+export async function merrymakeService(
+  handlers: { [action: string]: Handler | undefined },
+  init?: () => Promise<void>
+) {
+  const action = process.argv[process.argv.length - 2];
+  const handler = handlers[action];
+  if (handler !== undefined) {
+    const envelope: Envelope = JSON.parse(
+      process.argv[process.argv.length - 1]
+    );
+    handler(getPayload(), envelope);
+  } else if (init !== undefined) await init();
+}
+
+type RapidsResponse = AxiosResponse<any, any>;
+export function postToRapids(
+  event: "$reply",
+  payload: { content: any; mime: MimeType<string, string> }
+): Promise<RapidsResponse>;
+export function postToRapids(
+  event: "$join",
+  payload: {
+    content: string;
+    mime: MimeType<"text", "plain">;
+  }
+): Promise<RapidsResponse>;
+export function postToRapids(
+  event: "$broadcast",
+  payload: {
+    content: { to: string; event: string; payload: any };
+    mime: MimeType<"application", "json">;
+  }
+): Promise<RapidsResponse>;
+export function postToRapids(
+  event: "$send",
+  payload: {
+    content: { to: string; event: string; payload: any };
+    mime: MimeType<"application", "json">;
+  }
+): Promise<RapidsResponse>;
+export function postToRapids(
+  event: "$retrieve",
+  payload: {
+    content: {
+      file: string;
+      emit: string;
+      passthrough: any;
+    };
+    mime: MimeType<"application", "json">;
+  }
+): Promise<RapidsResponse>;
+export function postToRapids(
+  event: "$store",
+  payload: {
+    content: {
+      contents: Buffer[];
+      emit: string;
+      passthrough: any;
+    };
+    mime: MimeType<"application", "json">;
+  }
+): Promise<RapidsResponse>;
+export function postToRapids(
+  event: string,
+  payload?: { content: any; mime: MimeType<string, string> }
+): Promise<RapidsResponse>;
+export function postToRapids(
+  event: string,
+  payload?: { content: any; mime: MimeType<string, string> }
+) {
+  if (payload !== undefined) {
+    return axios.post(`${process.env.RAPIDS}/${event}`, payload.content, {
+      headers: { "Content-Type": payload.mime.toString() },
+    });
+  } else {
+    return axios.post(`${process.env.RAPIDS}/${event}`);
+  }
+}
+export function replyToOrigin(content: any, mime: MimeType<string, string>) {
+  return postToRapids("$reply", { content, mime });
+}
+export async function replyFileToOrigin(
+  path: string,
+  mime?: MimeType<string, string>
+) {
+  try {
+    let realMime =
+      mime !== undefined
+        ? mime
+        : optimisticMimeTypeOf(path.substring(path.lastIndexOf(".") + 1));
+    if (realMime === null) throw "Unknown file type. Add mimeType argument.";
+    await postToRapids("$reply", {
+      content: await fs.readFile(path),
+      mime: realMime,
+    });
+  } catch (e) {
+    throw e;
+  }
+}
+export function joinChannel(channel: string) {
+  return postToRapids("$join", {
+    content: channel,
+    mime: COMMON_MIME_TYPES.txt[0],
+  });
+}
+export function broadcastToChannel(to: string, event: string, payload: any) {
+  return postToRapids("$broadcast", {
+    content: { to, event, payload },
+    mime: COMMON_MIME_TYPES.json[0],
+  });
+}
+export function sendToClient(to: string, event: string, payload: any) {
+  return postToRapids("$send", {
+    content: { to, event, payload },
+    mime: COMMON_MIME_TYPES.json[0],
+  });
+}
+export function requestFileThenEmit(
+  file: string,
+  emitEvent: string,
+  passthrough: any
+) {
+  return postToRapids("$retrieve", {
+    content: { file, emit: emitEvent, passthrough },
+    mime: COMMON_MIME_TYPES.json[0],
+  });
+}
+export function storeFilesThenEmit(
+  contents: Buffer[],
+  emitEvent: string,
+  passthrough: any
+) {
+  return postToRapids("$store", {
+    content: { contents, emit: emitEvent, passthrough },
+    mime: COMMON_MIME_TYPES.json[0],
+  });
+}
+
+export async function parseUploadFileInfo<T>(p: Promise<Buffer>) {
+  let result: PayloadTypes.UploadFileInfo<T> = JSON.parse((await p).toString());
+  return result;
+}
+export async function parseRetrieveFileContent<T>(p: Promise<Buffer>) {
+  let result: PayloadTypes.FileContent<T> = JSON.parse((await p).toString());
+  return { ...result, content: Buffer.from(result.content.data) };
+}
+export async function parseStoreFileContent<T>(p: Promise<Buffer>) {
+  let result: PayloadTypes.StoreFileInfo<T> = JSON.parse((await p).toString());
+  return result;
+}
+
+function getPayload() {
+  return new Promise<Buffer>((resolve, reject) => {
+    let bufs: Buffer[] = [];
+    process.stdin.on("data", (data: Buffer) => {
+      bufs.push(data);
+    });
+    process.stdin.on("end", () => {
+      resolve(Buffer.concat(bufs));
+    });
+  });
+}
+
+function mapValues<T extends { [key: string]: any }>(
+  obj: T
+): { [key in keyof T]: T[key][0] } {
+  let result: { [key: string]: any } = {};
+  Object.keys(obj).forEach((k) => (result[k] = obj[k][0]));
+  return result as { [key in keyof T]: T[key] };
+}
+
+export const MIME_TYPES = mapValues(COMMON_MIME_TYPES);
